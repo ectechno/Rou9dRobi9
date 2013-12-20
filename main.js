f06235fb0a092b883ee49a758a175737109c5f5c
Hasith Yaggahavita <hasith@gmail.com>
2013-12-20 20:35:20 +0530

    initial code

---
+++ main.js
@@ -0,0 +1,98 @@
+﻿var app = angular.module('myApp', []);
+
+app.run(function ($rootScope) {
+    $rootScope.name = "Hasith";
+});
+
+app.controller('ItemsController', function ($scope, $rootScope, $http) {
+    $scope.curruntIndex = 0;
+
+    $rootScope.$on("TIMEOUT", function () {
+        $scope.curruntIndex += 1;
+    });
+
+    $scope.jumpTo = function (itemIndex) {
+        $scope.curruntIndex = itemIndex;
+        $rootScope.$broadcast("RESET_TIMER");
+    }
+
+    $http({
+        method: 'GET',
+        //url: 'http://99xtechnology.lk/services/api/Projects',
+        url: 'projects.js'
+    }).success(function (data, status) {
+        $scope.items = data;
+        $scope.itemsCol1 = data.slice(0, data.length/2);
+        $scope.itemsCol2 = data.slice(data.length / 2);
+
+        $rootScope.$broadcast("RESET_TIMER");
+    }).error(function (data, status) {
+        console.error("HTTP error : " + status);
+    });
+});
+
+app.controller('TimerController', function ($scope, $rootScope) {
+    $scope.timespan = 120;
+    $scope.countdown = $scope.timespan;
+    $scope.countdown -= 1;
+    $scope.timerPaused = true;
+
+    var beep = (function () {
+        var ctx = new (window.audioContext || window.webkitAudioContext);
+        return function (duration, type, finishedCallback) {
+
+            duration = +duration;
+
+            // Only 0-4 are valid types.
+            type = (type % 5) || 0;
+
+            if (typeof finishedCallback != "function") {
+                finishedCallback = function () { };
+            }
+
+            var osc = ctx.createOscillator();
+
+            osc.type = type;
+
+            osc.connect(ctx.destination);
+            osc.noteOn(0);
+
+            setTimeout(function () {
+                osc.noteOff(0);
+                finishedCallback();
+            }, duration);
+
+        };
+    })();
+   
+    var updateClock = function () {
+        $scope.countdown -= 1;
+        if ($scope.countdown < 10) {
+            beep(50, 3);
+        }
+        if ($scope.countdown <= 0) {
+            $rootScope.$broadcast("TIMEOUT");
+            $scope.countdown = $scope.timespan;
+        }
+    }
+
+    var timer = null;
+
+    $scope.resumeTimer = function () {
+        timer = setInterval(function () {
+            $scope.$apply(updateClock);
+        }, 1000);
+        $scope.timerPaused = false;
+    }
+ 
+    $scope.pauseTimer = function () {
+        clearInterval(timer);
+        $scope.timerPaused = true;
+    }
+
+    $rootScope.$on("RESET_TIMER", function () {
+        $scope.countdown = $scope.timespan;
+    });
+
+    
+});
---
+++ projects.js
@@ -0,0 +1,327 @@
+﻿[
+  {
+      "name": "Netpublish",
+      "rep": "prasathm",
+      "members": [
+        "anuraa"
+      ]
+  },
+  {
+      "name": "Research",
+      "rep": "dileepaj",
+      "members": [
+        "samiyurus",
+        "lukshicad"
+      ]
+  },
+  {
+      "name": "Puzzlepart",
+      "rep": "chaturad",
+      "members": [
+        "indikar",
+        "janakaj",
+        "inokas"
+      ]
+  },
+  {
+      "name": "SPN",
+      "rep": "prasathm",
+      "members": [
+        "ayanig",
+        "chamileb",
+        "charindraw",
+        "sajithp",
+        "rangithak"
+      ]
+  },
+  {
+      "name": "SuO",
+      "rep": "buddhinathaw",
+      "members": [
+        "mihiril",
+        "anushkaw",
+        "janithg",
+        "needras",
+        "tharindal",
+        "yusram"
+      ]
+  },
+  {
+      "name": "SuO Dev",
+      "rep": "buddhinathaw",
+      "members": [
+        "helaniw",
+        "kusalaw"
+      ]
+  },
+  {
+      "name": "Trio Link",
+      "rep": "chaturad",
+      "members": [
+        "jayakarant",
+        "chathurangab",
+        "janithp"
+      ]
+  },
+  {
+      "name": "BUS",
+      "rep": "buddhinathaw",
+      "members": [
+        "shivankad",
+        "kosalap",
+        "dinushab",
+        "modithad",
+        "nipunas",
+        "jayangaw",
+        "dananjayaw"
+      ]
+  },
+  {
+      "name": "Tempus",
+      "rep": "chaturad",
+      "members": [
+        "aradhanal",
+        "nirangad",
+        "ivanthas",
+        "erangau"
+      ]
+  },
+  {
+      "name": "Xtra",
+      "rep": "shehanis",
+      "members": [
+        "shiranthad",
+        "sudatht"
+      ]
+  },
+  {
+      "name": "Centric",
+      "rep": "shehanis",
+      "members": [
+        "yasithp",
+        "darshathaw",
+        "pacifikaa"
+      ]
+  },
+  {
+      "name": "Hatteland",
+      "rep": "prasathm",
+      "members": [
+        "sachintanad",
+        "samudrak",
+        "ranganar",
+        "chariths",
+        "jayanthas",
+        "lalindar",
+        "madushib"
+      ]
+  },
+  {
+      "name": "Whatif",
+      "rep": "hasithy",
+      "members": [
+        "ashanf",
+        "mohammedm",
+        "rivindup",
+        "asankan",
+        "manoshas"
+      ]
+  },
+  {
+      "name": "eDok",
+      "rep": "shehanis",
+      "members": [
+        "sameerag",
+        "yohanw",
+        "buddhikap"
+      ]
+  },
+  {
+      "name": "Adramatch",
+      "rep": "prasathm",
+      "members": [
+        "amilad",
+        "rakhithac",
+        "geethangaa",
+        "thilokg",
+        "sarangal",
+        "tharinduf",
+        "frankw",
+        "asankaf",
+        "sandrinaa",
+        "isharap",
+        "shamikad"
+      ]
+  },
+  {
+      "name": "Mobile",
+      "rep": "dileepaj",
+      "members": [
+        "sabrym",
+        "tharindraj"
+      ]
+  },
+  {
+      "name": "Nordeca Boat",
+      "rep": "dileepaj",
+      "members": [
+        "janiduw"
+      ]
+  },
+  {
+      "name": "Nordeca LI",
+      "rep": "hasithy",
+      "members": [
+        "dhammikas",
+        "chinthakad"
+      ]
+  },
+  {
+      "name": "Page planner",
+      "rep": "dileepaj",
+      "members": [
+        "dileepaj",
+        "lolithaw"
+      ]
+  },
+  {
+      "name": "Facilit",
+      "rep": "chaturad",
+      "members": [
+        "kalanaw",
+        "malshanin",
+        "raminduw",
+        "surenr"
+      ]
+  },
+  {
+      "name": "Dodo",
+      "rep": "prasathm",
+      "members": [
+        "rajindar"
+      ]
+  },
+  {
+      "name": "CloudHub",
+      "rep": "hasithy",
+      "members": [
+        "niranjank",
+        "lasanthab",
+        "isurus"
+      ]
+  },
+  {
+      "name": "CAMMS",
+      "rep": "prasathm",
+      "members": [
+        "sachithp",
+        "gayanit",
+        "akalankah"
+      ]
+  },
+  {
+      "name": "Riftlabs",
+      "rep": "dileepaj",
+      "members": [
+        "sukhithaj",
+        "dinushaa",
+        "dilithav",
+        "krishnakripaj"
+      ]
+  },
+  {
+      "name": "Compello",
+      "rep": "chamindaw",
+      "members": [
+        "kishann",
+        "chamindav",
+        "surangab"
+      ]
+  },
+  {
+      "name": "MIS/Lankagate",
+      "rep": "chaturad",
+      "members": [
+        "hashanw"
+      ]
+  },
+  {
+      "name": "Frameworks/Tools",
+      "rep": "hasithy",
+      "members": [
+        "dushanthar",
+        "udithg",
+        "hashnis",
+        "supunn",
+        "yasurav"
+      ]
+  },
+  {
+      "name": "UI team",
+      "rep": "buddhinathaw",
+      "members": [
+        "nishadr",
+        "hasangaa",
+        "sandunf",
+        "cristofferc",
+        "rasikam"
+      ]
+  },
+  {
+      "name": "Shared",
+      "rep": "shehanis",
+      "members": [
+        "dilanw",
+        "hasithy",
+        "shehanis",
+        "prasathm",
+        "chaturad",
+        "rumeshw",
+        "ashaffh",
+        "buddhinathaw",
+        "rositad",
+        "anujah"
+      ]
+  },
+  {
+      "name": "Admin",
+      "rep": "shehanis",
+      "members": [
+        "mano",
+        "rifhanm",
+        "revathyj",
+        "shenikah",
+        "dilanib",
+        "nilminir",
+        "anilg",
+        "hirans",
+        "sahans",
+        "upalis",
+        "thilakw",
+        "pasank",
+        "personalassistant",
+        "nalinn",
+        "cassandram"
+      ]
+  },
+  {
+      "name": "Mngt trainees",
+      "rep": "shehanis",
+      "members": [
+        "shanu",
+        "navakan"
+      ]
+  },
+  {
+      "name": "To be Allocated",
+      "rep": "shehanis",
+      "members": [
+        "sabry",
+        "janith",
+        "arshad",
+        "kalanamith",
+        "luckshica",
+        "prasadi"
+      ]
+  }
+]
